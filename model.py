99117e1a0aad6b4452eb4eb8281c44a48a4c0427
Abhijith PM <apm20055@gmail.com>
2026-02-19 11:57:42 +0530

    added models

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import joblib
 import pandas as pd
 
-df=pd.read_csv('final_ml_dataset.csv')
+df = pd.read_csv("final_ml_dataset.csv")
 
 X = df.drop(columns=["label"])
 y = df["label"]
@@ -12,15 +13,17 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# Scale
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Train
 model = RandomForestClassifier()
-model.fit(X_train, y_train)
+model.fit(X_train_scaled, y_train)
 
-pred = model.predict(X_test)
+# Save
+joblib.dump(model, "behavior_model.pkl")
+joblib.dump(scaler, "scaler.pkl")
 
-from sklearn.model_selection import cross_val_score
-
-scores = cross_val_score(model, X, y, cv=10)
-print(scores)
-print("Average:", scores.mean())
-
-
+print("Saved successfully.")
